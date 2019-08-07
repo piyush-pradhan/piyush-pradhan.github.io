b319d9249c2017f6ae404872cba40f62f8ca0c67
Piyush Pradhan <piyushpradhan@hotmail.com>
2019-08-07 18:19:45 +0530

    Side navigation customized to display icons. Responsive side navigation added.

--- sustain_online/assets/js/custom_support.js
+++ sustain_online/assets/js/custom_support.js
@@ -49,59 +49,136 @@ $(document).ready(function() {
 
            $('[data-toggle="popover"]').popover();
 
+           $("#resp-sidebar").mCustomScrollbar({
+                theme: "minimal"
+            });
+
+            $('#dismiss, .overlay').on('click', function () {
+                $('#resp-sidebar').removeClass('active');
+                $('.overlay').removeClass('active');
+            });
+
+            $('#resp-sidebarCollapse').on('click', function () {
+                $('#resp-sidebar').addClass('active');
+                $('.overlay').addClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            });
+
+
+
+            $('.fa-bars').click(function() {
+        if ($('#sidebar').hasClass("half-nav")) {
+            $('#main-content').css({
+                'margin-left': '210px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar > ul').show();
+            $('#sidebar').css({
+                'margin-left': '0',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar').css({
+                'width': '210px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar > ul').css({
+                'width': '210px',
+                'transition': 'all 0.9s ease'
+            });
+            $("#sidebar").removeClass("half-nav");
+            $('ul.sidebar-menu > li > a:hover').css({
+                'border-radius': '0'
+            });
+            $('ul.sidebar-menu > li > a > span').css({
+                'display': 'inline-block'
+            });
+            $('ul.sidebar-menu > li > a > i').css({
+                'font-size': '15px',
+                'transition': 'all 0.9s ease'
+            });
+            $('a.active').css({
+                'border-radius': '0%'
+            });
+            $('ul.sidebar-menu > li > a > i').css({
+                'font-size': '15px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar > ul > li').css({
+                'padding': '0px'
+            });
+            $('ul.sidebar-menu.second > li > p').css({
+                'display': 'block',
+                'transition': 'all 0.9s ease'
+            });
+            $('ul.sidebar-menu.second hr').css({
+                'display': 'block',
+                'transition': 'all 0.9s ease'
+            });
+            $('ul.sidebar-menu.third ').css({
+                'display': 'block',
+                'transition': 'all 0.9s ease'
+            });
+        } else {
+            $('#main-content').css({
+                'margin-left': '60px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar').css({
+                'margin-left': '0px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar > ul').show();
+            $('#sidebar > ul').css({
+                'width': '55px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar').css({
+                'width': '55px',
+                'transition': 'all 0.9s ease'
+            });
+            $('#sidebar').addClass("half-nav");
+            $('ul.sidebar-menu > li > a').css({
+                'padding': '3px 0 3px 10px',
+                'border-radius': '0%'
+            });
+            $('ul.sidebar-menu > li > a.active').css({
+                'padding': '3px 0 3px 7px',
+                'border-radius': '50%'
+            });
+            $('ul.sidebar-menu > li > a:hover').css({
+                'padding': '3px 0 3px 7px',
+                'border-radius': '0%'
+            });
+            $('ul.sidebar-menu > li > a span').css({
+                'display': 'none',
+                'transition': 'all 0.9s ease'
+            });
+            $('ul.sidebar-menu > li > a > i').css({
+                'font-size': '20px',
+                'transition': 'all 0.9s ease',
+                'padding': '6px 0px'
+            });
+            $('#sidebar > ul > li').css({
+                'padding': '0 8px'
+            });
+            $('a.active').css({
+                'border-radius': '50%'
+            });
+            $('ul.sidebar-menu.second > li > p').css({
+                'display': 'none',
+                'transition': 'all 0.9s ease'
+            });
+            $('ul.sidebar-menu.second hr').css({
+                'display': 'none',
+                'transition': 'all 0.9s ease'
+            });
+            $('ul.sidebar-menu.third ').css({
+                'display': 'none',
+                'transition': 'all 0.9s ease'
+            });
+        }
+    });
+
       })
 
-$document.ready(function()
-{
-  var drop = $("input");
-          drop.on('dragenter', function (e) {
-            $(".drop").css({
-              "border": "4px dashed #09f",
-              "background": "#f4f4f4"
-            });
-            $(".cont").css({
-              "color": "#09f"
-            });
-          }).on('dragleave dragend mouseout drop', function (e) {
-            $(".drop").css({
-              "border": "3px dashed #DADFE3",
-              "background": "transparent"
-            });
-            $(".cont").css({
-              "color": "#8E99A5"
-            });
-          });
-
-
-
-          function handleFileSelect(evt) {
-            var files = evt.target.files; // FileList object
-
-            // Loop through the FileList and render image files as thumbnails.
-            for (var i = 0, f; f = files[i]; i++) {
-
-              // Only process image files.
-              if (!f.type.match('image.*')) {
-                continue;
-              }
-
-              var reader = new FileReader();
-
-              // Closure to capture the file information.
-              reader.onload = (function(theFile) {
-                return function(e) {
-                  // Render thumbnail.
-                  var span = document.createElement('span');
-                  span.innerHTML = ['<img class="thumb" src="', e.target.result,
-                                    '" title="', escape(theFile.name), '"/>'].join('');
-                  document.getElementById('list').insertBefore(span, null);
-                };
-              })(f);
-
-              // Read in the image file as a data URL.
-              reader.readAsDataURL(f);
-            }
-          }
-
-          $('#files').change(handleFileSelect);
-})
